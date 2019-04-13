837abb85c641748acc337bd9f1a8f66ee50aa497
cgbotta <cgbotta@ncsu.edu>
2019-04-13 19:49:18 -0400

    Added new DBs

--- app/src/main/java/com/example/packhacks2019/db/TaskContract.java
+++ app/src/main/java/com/example/packhacks2019/db/TaskContract.java
@@ -3,8 +3,6 @@ package com.example.packhacks2019.db;
 import android.provider.BaseColumns;
 
 public class TaskContract {
-    public static final String DB_NAME = "com.example.packhacks2019.db";
-    public static final int DB_VERSION = 1;
 
     public class TaskEntry implements BaseColumns {
         public static final String TABLE = "cards";
